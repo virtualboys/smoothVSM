e1ca7e42b12c54e37fc0b2ff924c92007a5da939
yung blood <yung@yung-S500CA.(none)>
2014-04-02 13:46:17 -0400

    hello

--- loadShader.h
+++ loadShader.h
@@ -5,12 +5,15 @@
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
-#include <GL/glut.h>
-#endif
 
-#include <iostream>
-#include <fstream>
-#include <vector>
+#define GL_GLEXT_PROTOTYPES
+
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <GL/glu.h>
+#include <GL/glext.h>
+#include <GL/glxext.h>
+#endif
 
 
 GLuint loadShaders(const char * vertex_file_path,const char * fragment_file_path);
